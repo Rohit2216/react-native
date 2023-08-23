295e83b253deed2e03be27aac300206292eb3dfa
Rohit2216 <chauhanrohit716@gmail.com>
2023-08-24 00:37:08 +0530

    loginlogout done

---
+++ loginlogout/app/index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import Login from './Login'; // Import your Login component
+import Register from './Register'; // Import your Register component
+
+// Register your components
+AppRegistry.registerComponent('Login', () => Login);
+AppRegistry.registerComponent('Register', () => Register);
